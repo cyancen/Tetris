98f6e0905ec35283fee32415560d99b313a03d00
Cyril Ancen <cyr17ancen@gmail.com>
2022-10-06 23:16:33 +1000

    Tetris Main

---
+++ Assets/Scripts/Boxes.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boxes : MonoBehaviour {
+
+	float fall = 0;
+	public float fallSpeed = 1;
+	public bool allowRotation = true;
+	public bool limitRotation = false;
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		CheckUserInput();
+	}
+
+	void CheckUserInput () {
+
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			transform.position += new Vector3(1,0,0);
+			if (CheckIsValidPosition()) {
+
+			} else {
+				transform.position += new Vector3(-1,0,0);
+			}
+
+		} else if (Input.GetKeyDown(KeyCode.LeftArrow)) 
+		{
+			transform.position += new Vector3(-1,0,0);
+			if (CheckIsValidPosition()) {
+
+			} else {
+				transform.position += new Vector3(1,0,0);
+			}
+		}
+		 else if (Input.GetKeyDown(KeyCode.Space))
+		{ 
+			 transform.Rotate (0,0,90);
+			 if (CheckIsValidPosition()) {
+
+			 } else {
+				 transform.Rotate (0,0,-90);
+			 }
+		}
+		 else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed) 
+		{
+			transform.position += new Vector3(0,-1,0);
+
+			fall = Time.time;
+
+			if (CheckIsValidPosition()) {
+
+			} else {
+				transform.position += new Vector3(0,1,0);
+			}
+		}
+	}
+
+	bool CheckIsValidPosition () {
+		foreach (Transform box in transform) {
+			Vector2 pos = FindObjectOfType<Game>().Round (box.position);
+
+			if (FindObjectOfType<Game>().CheckInsideGrid (pos) == false) {
+				return false;
+			} 
+		}
+		return true;
+	}	
+
+
+
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	public static int gridWidth = 10;
+	public static int gridHeight = 20;
+
+
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+
+	public bool CheckInsideGrid (Vector2 pos) {
+		return ((int)pos.x >=0 && (int)pos.x < gridWidth && (int)pos.y >=0);
+	}
+
+	public Vector2 Round (Vector2 pos) {
+		return new Vector2 (Mathf.Round(pos.x), Mathf.Round(pos.y));
+	}
+
+
+}
